d13f93a2098c15079f545d1296145a5b1f74c266
Jorge Soto <Jorgemsm19@gmail.com>
2019-08-09 15:10:55 -0400

    Create user Register page

---
+++ app/javascript/components/Forms/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { Grid, TextField, InputAdornment, Button } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
+import KeyIcon from '@material-ui/icons/VpnKey';
+
+import useRegisterFormStyles from './styles';
+
+interface Props {
+  values: { [field: string]: any };
+  errors: { [field: string]: any };
+  handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  submit: (event?: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const RegisterForm: React.FC<Props> = ({ values, errors, handleChange, submit }) => {
+  const classNames = useRegisterFormStyles({});
+
+  return (
+    <Grid container spacing={1} direction="column" className={classNames.fromGridContainer}>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Name"
+          name="name"
+          margin="normal"
+          variant="outlined"
+          autoComplete="name"
+          value={values.name}
+          error={!!errors.name}
+          helperText={errors.name}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonIcon className={classNames.formFieldIcon} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Email"
+          type="email"
+          name="email"
+          margin="normal"
+          variant="outlined"
+          autoComplete="email"
+          error={!!errors.email}
+          helperText={errors.email}
+          value={values.email}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <MailIcon className={classNames.formFieldIcon} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Password"
+          type="password"
+          name="password"
+          margin="normal"
+          variant="outlined"
+          autoComplete="off"
+          error={!!errors.password}
+          helperText={errors.password}
+          value={values.password}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <KeyIcon className={classNames.formFieldIcon} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          fullWidth
+          label="Confirm password"
+          type="password"
+          name="password_confirmation"
+          margin="normal"
+          variant="outlined"
+          autoComplete="off"
+          error={!!errors.password_confirmation}
+          helperText={errors.password_confirmation}
+          value={values.password_confirmation}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <KeyIcon className={classNames.formFieldIcon} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <Button color="primary" variant="contained" fullWidth onClick={() => submit()}>
+          Register
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default RegisterForm;
--- app/javascript/pages/UnauthenticatedApp/RegisterPage/RegisterPage.tsx
+++ app/javascript/pages/UnauthenticatedApp/RegisterPage/RegisterPage.tsx
@@ -2,14 +2,38 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Link } from '@material-ui/core';
 
+import useForm from '../../../hooks/useForm';
+import { RegisterFormSetup } from '../../../utils/validation';
+
+import RegisterForm from '../../../components/Forms/RegisterForm';
+
 const RegisterPage: React.FC = () => {
+  const { initialValues, validate } = RegisterFormSetup;
+  const { values, errors, handleChange, handleSubmit } = useForm(
+    undefined,
+    validate,
+    initialValues,
+  );
+
   return (
-    <div>
-      {/* Register Page */}
-      <Link component={RouterLink} to="/" color="primary" underline="none">
+    <React.Fragment>
+      <RegisterForm
+        values={values}
+        errors={errors}
+        handleChange={handleChange}
+        submit={handleSubmit}
+      />
+
+      <Link
+        component={RouterLink}
+        to="/"
+        color="primary"
+        underline="none"
+        style={{ marginTop: 16 }}
+      >
         Back to Login
       </Link>
-    </div>
+    </React.Fragment>
   );
 };
 
