c917e73427057aa15ca3540c17102374e90074dc
Jorge Soto <Jorgemsm19@gmail.com>
2019-08-10 15:04:31 -0400

    Fix: Session domain on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
-if Rails.env === 'production'
+if Rails.env.production?
   Rails.application.config.session_store :cookie_store,
                                          key: '_authentication',
                                          domain:
-                                           'https://manten-rails-finance.herokuapp.com'
+                                           'manten-rails-finance.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication'
 end
